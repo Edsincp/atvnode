6658fa09d7a22ae69fd8f8f0dfa662ac329e18fe
edsinn <edsonpereiracarvalho10@gmail.com>
2024-03-21 10:38:08 -0300

    Ativdade em node em andamento

--- tapedson/tapedson/src/App.js
+++ tapedson/tapedson/src/App.js
@@ -1,24 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Inicio from './inicio';
 import logo from './aston2.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* Logo centralizada */}
-        <div className="App-logo">
-          <img src={logo} alt="logo" />
-        </div>
-       
-        <h1>Aston Martin</h1>
-       
-        <p className="Bold-text">No horizonte brilhante da estrada da vida, reside o desejo ardente de sentir a emoção única de dirigir um Aston Martin.</p>
-        <p className="Bold-text">Aston Martin: Potência inigualável.</p>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <div className="App-logo">
+            <img src={logo} alt="logo" />
+          </div>
+         
+          <h1>Aston Martin</h1>
+         
+          <p className="Bold-text">No horizonte brilhante da estrada da vida, reside o desejo ardente de sentir a emoção única de dirigir um Aston Martin.</p>
+          <p className="Bold-text">Aston Martin: Potência inigualável.</p>
 
-        {/* Botão centralizado */}
-        <button className="Register-button">Registrar-se</button>
-      </header>
-    </div>
+          <Link to="/inicio" className="Register-button">Registrar-se</Link>
+
+          <Route path="/inicio" component={Inicio} />
+        </header>
+      </div>
+    </Router>
   );
 }
 
